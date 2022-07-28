396b60b2a9f292335f45418788810450bd6b3eef
hizkielT <hezekiel.feyissa@gmail.com>
2022-07-28 20:40:32 +0300

    Modified the dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './screens/navbar';
 import Footer from './screens/footer';
+import Dashboard from './screens/dashboard';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Dashboard />
       <Footer />
     </>
   );
--- client/src/screens/dashboard.jsx
+++ client/src/screens/dashboard.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { Typography, Grid, Paper, 
+    ThemeProvider, Box, Container, createTheme } from '@mui/material'
 
+const mdTheme = createTheme()
 
 function Dashboard(props) {
     return (
-        <div>
-            
-        </div>
+        <ThemeProvider theme={mdTheme}>
+            <Box
+                component="main"
+                sx={{
+                    backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[900],
+                    flexGrow: 1,
+                    height: '83.6vh',
+                    overflow: 'auto'
+                }}
+            >
+                <Container maxWidth="lg" sx={{ mt:4, mb: 4, }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={12} lg={12}>
+                            <Paper
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    mt:-1, mb: 4,
+                                }}
+                            >
+                                <Typography component="h1" variant="h5" sx={{ mt: 4,mb: 4,textAlign: 'center'}}>
+                                    Upcoming Conventions
+                                </Typography>  
+                            </Paper>
+                            <Grid container></Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </ThemeProvider>
     );
 }
 
