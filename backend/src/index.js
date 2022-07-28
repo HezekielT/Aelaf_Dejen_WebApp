3a56d50a79552faba0421e06e807ee8cbf7af5a7
hizkielT <hezekiel.feyissa@gmail.com>
2022-07-28 13:12:37 +0300

    created our server

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const app = express();
+
+const DATABASE_CONNECTION = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_CONNECTION, {
+    UseNewUrlParser: true,
+});
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res ) => {
+    res.json({
+        message: 'Hello There',
+    });
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Currently Listening at http://localhost:${port}`);
+});
