4489197f538307e871141d0c83d00a9711c632fb
hizkielT <hezekiel.feyissa@gmail.com>
2022-07-29 19:13:20 +0300

    created manage web content component

--- client/src/components/Admin/manage_web_content.js
+++ client/src/components/Admin/manage_web_content.js
@@ -1,10 +1,34 @@
+import { Grid, Card, CardContent, Typography } from '@mui/material';
 import React from 'react';
+import Events from '../Events';
 
 function Manage_Contents(props) {
     return (
-        <div>
+        <>
+            <Grid maxWidth="lg" container spacing={3}>
+            <Grid item>
+            <Events />
+            </Grid>
+            <Grid item>
+            <Card
+                sx={{
+                    display: 'flex',
+                    mt: 4, mb: 4,
+                }}
+            >
+                {/* <CardMedia 
+                    component="img"
+                /> */}
+                <CardContent sx={{ flex:1 }}>
+                    <Typography component="h1" variant="h5">
+                        jgorAnnual Meeting starting from Aug. 3
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
+            </Grid>
             
-        </div>
+        </>
     );
 }
 
