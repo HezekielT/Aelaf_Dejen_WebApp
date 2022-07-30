2f954ddee768c2518a1de8716932eaeff74ee32c
hizkielT <hezekiel.feyissa@gmail.com>
2022-07-30 21:47:44 +0300

    created our first route file

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,55 @@
+const { Router } = require("express")
+import { Participant, Driver } from '../models/user.model';
+
+
+const router = Router();
+
+router.route('/participate').post(async function(req, res) {
+    const { first_name, last_name, email, phoneno, address } = req.body;
+    try {
+        await Participant.create({
+            user: {
+                first_name,
+                last_name,
+                email,
+                phoneno,
+            },
+            address,
+        });
+        console.log("Successfully registered");
+        res.status(201).send();
+    } catch(err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+router.route('/addEvent').post(async function(req, res) {
+    const { title, description, dateTime, location } = req.body;
+})
+router.get("/register", async (req, res, next) => {
+    try {
+        const entries = await LogEntry.find();
+        res.json(entries);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try {
+        if (req.get("X-API-KEY") !== API_KEY) {
+            res.status(401);
+            throw new Error("Unauthorized Access");
+        }
+        const logEntry = new LogEntry(req.body);
+        const createdEntry = await logEntry.save();
+        res.json(createdEntry);
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            res.status(422);
+        }
+        next(error);
+    }
+});
+
+module.exports = router;
