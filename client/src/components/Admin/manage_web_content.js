785ffbf0054c3ebacd1046990c4359b88b500598
hizkielT <hezekiel.feyissa@gmail.com>
2022-07-30 10:38:39 +0300

    modification to manage web content

--- client/src/components/Admin/manage_web_content.js
+++ client/src/components/Admin/manage_web_content.js
@@ -1,34 +1,43 @@
-import { Grid, Card, CardContent, Typography } from '@mui/material';
-import React from 'react';
+import { Grid, Card, CardContent, Typography, CardActions } from '@mui/material';
+import React, { useState } from 'react';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import Events from '../Events';
 
 function Manage_Contents(props) {
+    const [test, setTest] = useState("Hello")
     return (
-        <>
+        <div>
             <Grid maxWidth="lg" container spacing={3}>
-            <Grid item>
-            <Events />
-            </Grid>
-            <Grid item>
-            <Card
-                sx={{
-                    display: 'flex',
-                    mt: 4, mb: 4,
-                }}
-            >
-                {/* <CardMedia 
-                    component="img"
-                /> */}
-                <CardContent sx={{ flex:1 }}>
-                    <Typography component="h1" variant="h5">
-                        jgorAnnual Meeting starting from Aug. 3
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Grid>
+                <Grid item>
+                    <Events />
+                </Grid>
+                <Grid item>
+                    <Card
+                        sx={{
+                            cursor: 'pointer',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            mt: 4, mb: 4,
+                        }}
+                    >
+                        {/* <CardMedia 
+                            component="img"
+                        /> */}
+                        <CardContent sx={{ flex:1 }}>
+                            <Typography component="h1" variant="h5">
+                                Add New Convention
+                            </Typography>
+                            
+                        </CardContent>
+                        <CardActions  sx={{ justifyContent: 'center'}}>
+                            <ControlPointIcon />
+                            {/* {test} */}
+                        </CardActions>
+                    </Card>
+                </Grid>
             </Grid>
             
-        </>
+        </div>
     );
 }
 
