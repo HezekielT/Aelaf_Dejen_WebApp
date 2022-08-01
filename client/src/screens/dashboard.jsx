5b470bc642e5b3ac9479057fb9eb8d011a2764bd
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-01 17:41:37 +0300

    change to dashboard

--- client/src/screens/dashboard.jsx
+++ client/src/screens/dashboard.jsx
@@ -2,47 +2,20 @@ import React from 'react';
 import { Typography, Grid, Paper, 
     ThemeProvider, Box, Container, createTheme } from '@mui/material'
 import Participant_Regist from '../components/Participant-Registration';
+import Events from '../components/Events';
+import Home from '../components/Home';
+import ContactUs from '../components/ContactUs';
 
 const mdTheme = createTheme()
 
 function Dashboard(props) {
     return (
         <ThemeProvider theme={mdTheme}>
-            <Box
-                component="main"
-                sx={{
-                    backgroundColor: (theme) =>
-                    theme.palette.mode === 'light'
-                    ? theme.palette.grey[100]
-                    : theme.palette.grey[900],
-                    flexGrow: 1,
-                    height: '83.6vh',
-                    overflow: 'auto'
-                }}
-            >
-                <Container maxWidth="lg" sx={{ mt:4, mb: 4, }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={12} lg={12}>
-                            <Paper
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    mt:-1, mb: 4,
-                                }}
-                            >
-                                <Typography component="h1" variant="h5" sx={{ mt: 4,mb: 4,textAlign: 'center'}}>
-                                    Upcoming Conventions
-                                </Typography>  
-                            </Paper>
-                            <Grid container spacing={2}>
-                                <Grid item lg={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-                                    <Participant_Regist />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Box>
+
+            <Home />
+            <Events />
+            <ContactUs />
+            
         </ThemeProvider>
     );
 }
