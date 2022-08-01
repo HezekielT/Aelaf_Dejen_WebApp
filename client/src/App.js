e82c9c3392e489c6f1af6aefbf3f39134d2cd88c
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-01 17:39:57 +0300

    App.js modified

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Dashboard from './screens/dashboard';
 import Admin_Dashboard from './screens/Admin_Dashboard';
 import Manage_Contents from './components/Admin/manage_web_content';
 import Manage_Account from './components/Admin/Manage_Account';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
@@ -13,8 +15,8 @@ function App() {
       <NavBar />
       {/* <Admin_Dashboard /> */}
       {/* <Manage_Contents /> */}
-      <Manage_Account />
-      {/* <Dashboard /> */}
+      <Dashboard />
+      {/* <Manage_Account /> */}
       <Footer />
     </>
   );
