33c0e3e2796eccc80a44629a6b38f0846c25d78d
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-02 13:47:08 +0300

    some modification to app

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,22 @@ import Manage_Contents from './components/Admin/manage_web_content';
 import Manage_Account from './components/Admin/Manage_Account';
 import Home from './components/Home';
 import ContactUs from './components/ContactUs';
+import Navigator from './Navigator';
+import { Outlet } from 'react-router-dom';
+import { RefProvider } from './context/refProvider';
 
 function App() {
   return (
-    <>
-      <NavBar />
+    <RefProvider>
+      {/* <Navigator /> */}
+      {/* <NavBar /> */}
       {/* <Admin_Dashboard /> */}
       {/* <Manage_Contents /> */}
-      <Dashboard />
+      {/* <Dashboard /> */}
       {/* <Manage_Account /> */}
-      <Footer />
-    </>
+      {/* <Footer /> */}
+      <Admin_Dashboard />
+    </RefProvider>
   );
 }
 
