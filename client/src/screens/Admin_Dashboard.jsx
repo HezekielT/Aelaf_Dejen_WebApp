4c7f40153d2efea1bfee431c7b00aeeb912ae42a
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-03 19:14:50 +0300

    modified admin dashboard

--- client/src/screens/Admin_Dashboard.jsx
+++ client/src/screens/Admin_Dashboard.jsx
@@ -16,7 +16,7 @@ function AdminDashboard(props) {
                     ? theme.palette.grey[100]
                     : theme.palette.grey[900],
                     flexGrow: 1,
-                    height: '100vh',
+                    maxHeight: '100vh',
                     overflow: 'auto'
                 }}
             >
