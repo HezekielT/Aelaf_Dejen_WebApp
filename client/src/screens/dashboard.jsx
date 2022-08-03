9204d3da6f9b3fbc87b5fb6f20e6276f2092fc85
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-03 19:14:34 +0300

    modified dashboard

--- client/src/screens/dashboard.jsx
+++ client/src/screens/dashboard.jsx
@@ -1,31 +1,62 @@
-import React, { useRef } from 'react';
-import { Typography, Grid, Paper, 
+import React from 'react';
+import { Typography, Grid, Paper,
     ThemeProvider, Box, Container, createTheme } from '@mui/material'
-import Participant_Regist from '../components/Participant-Registration';
 import Events from '../components/Events';
 import Home from '../components/Home';
 import ContactUs from '../components/ContactUs';
 
+
+import { useReference } from '../context/refProvider';
 const mdTheme = createTheme()
 
 function Dashboard(props) {
+    const { eventRef } = useReference();
 
-    // const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
-
-    // const homeRef = useRef(null)
-    // const eventRef = useRef(null)
-    // const contact = useRef(null)
-
-    // const executeScroll = (refe) => scrollToRef(refe)
     return (
         <ThemeProvider theme={mdTheme}>
 
             <Home />
-            <Events />
+            <Box
+                ref={eventRef}
+                component="main"
+                sx={{
+                    backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[900],
+                    flexGrow: 1,
+                    minHeight: '90.6vh',
+                    overflow: 'auto',
+                }}
+            >
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={12} lg={12} sx={{ mt: 2}}>
+                        <Paper
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                mt:5, mb: 4,
+                            }}
+                        >
+                            <Typography component="h1" variant="h5" sx={{ mt: 4,mb: 4,textAlign: 'center'}}>
+                                Upcoming Conventions
+                            </Typography>  
+                        </Paper>
+                        <Container>
+                            <Grid item xs={12} md={12} lg={12} sx={{ my: 3, justifyContent: 'center' }}>
+                                <Events admin={false}/>
+                            </Grid>
+                        </Container>
+                    </Grid>
+                </Grid>
+            </Box>
             <ContactUs />
-
         </ThemeProvider>
     );
 }
 
 export default Dashboard;
+
+
+
+
