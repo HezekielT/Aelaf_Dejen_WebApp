1a1603ce4b6884a8901483a2429fe1fdca8d59f8
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-05 21:59:15 +0300

    some changes to navigator

--- client/src/Navigator.js
+++ client/src/Navigator.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Admin_Options from './components/Admin/Admin_Options';
+import Manage_Account from './components/Admin/Manage_Account';
+import Manage_Contents from './components/Admin/manage_web_content';
+import Transport_Registration from './components/Admin/Transport_Registration';
 import AdminDashboard from './screens/Admin_Dashboard';
 import Dashboard from './screens/dashboard';
 import Footer from './screens/footer';
@@ -9,10 +13,15 @@ function Navigator(props) {
 
     return (
         <Router>
-            <NavBar />
+            {/* <NavBar /> */}
             <Routes>
                 <Route exact path="/" element={<Dashboard />} />
-                <Route path="/admin" element={<AdminDashboard />} /></Routes>
+                <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="/dashboard" element={<Admin_Options />} />
+                <Route path="/dashboard/contents" element={<Manage_Contents />} />
+                <Route path="/dashboard/transport" element={<Transport_Registration />} />
+                <Route path="/dashboard/accounts" element={<Manage_Account />} />
+            </Routes>
             <Footer />
         </Router>
     );
