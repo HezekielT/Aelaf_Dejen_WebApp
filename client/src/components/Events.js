f15f8808141102dd3b0cd37269d29cfddc5521ae
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-06 14:30:47 +0300

    wrote a function that gets conventions from db

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from '@mui/material/styles';
 import { Card, CardActions, CardContent,
     IconButton, Typography, Collapse, TextField } from '@mui/material'
@@ -8,6 +8,7 @@ import View_Participants from './View_Participants';
 // we will pass the contents of the event as
 // props to this function and will call it from
 // dashboard screen
+const axios = require('axios');
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -22,62 +23,83 @@ const ExpandMore = styled((props) => {
 function Events(props) {
     const val= props.admin
     const [expanded, setExpanded] = React.useState(false);
+    const [conventions, setConventions] = useState('');
+    const [admin, setAdmin] = useState(null)
+    const [text, setText] = useState('')
+    const config = {
+        header: {
+            "Content-Type": "application/json",
+        },
+    };
+    // get a list of conventions from our database
+    useEffect(async () => {
+        await axios.post(
+            "http://localhost:5000/getEvents",
+            config
+        )
+        .then(function (response){
+            setConventions(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        
+    }, [])
 
+    useEffect(() => {
+        if(val !== true) {
+            setText("REGISTER")
+            setAdmin(<Participant_Regist />)
+        } else {
+            setText("VIEW REGISTERED PARTICIPANTS")
+            setAdmin(<View_Participants />)
+        }
+            
+    },[])
     const handleExpandClick = () => {
         setExpanded(!expanded);
     }
+
     return ( 
         <Card>
-            <CardContent sx={{ flex:1 }}>
-                <Typography component="h1" variant="h5">
-                    Annual Meeting starting from Aug. 3
-                </Typography>
-                <TextField variant="outlined" disabled label="Annual Meeting starting from Aug. 3">
-                </TextField>
-            </CardContent>
-            {val !== true ? (
-                <React.Fragment>
-                    
-            <CardActions sx={{ border: 1, }}>
-                <Typography component="h1" variant="h5">
-                    REGISTER
-                </Typography>
-                <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded="Register"
-                >
-                <ExpandMoreIcon />
-                </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Participant_Regist />
-                </CardContent>
-            </Collapse>
-                </React.Fragment>
-            ): (
-                <React.Fragment>
-                    
-                <CardActions sx={{ border: 1 }}>
-                    <Typography component="h1" variant="h5">
-                        View Participants
+            <CardContent>
+            
+                {conventions !== null ? (
+                    conventions.map(convention => 
+                        // add an image
+                        <>
+                        <Typography component="h1" variant="h5" sx={{ justifyContent: 'center'}}>
+                            {convention.title}
+                        </Typography>
+                        <Typography paragraph>{convention.description}</Typography>
+                        <Typography paragraph>Date and Time: {convention.dateTime}</Typography>
+                        <Typography paragraph>Place: {convention.location}</Typography>
+                        </>
+                    )
+                ): (
+                    <Typography component="h1" variant="h5" sx={{ justifyContent: 'center'}}>
+                        There is no upcoming convention!
                     </Typography>
-                    <ExpandMore
-                        expand={expanded}
-                        onClick={handleExpandClick}
-                        aria-expanded="participant"
-                    >
-                    <ExpandMoreIcon />
-                    </ExpandMore>
-                </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                        <View_Participants />
+                )}
                     </CardContent>
-                </Collapse>
-                    </React.Fragment>
-            )}
+                <CardActions sx={{ border: 1, }}>
+            <Typography component="h1" variant="h5">
+                {text}
+            </Typography>
+            <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={text}
+            >
+            <ExpandMoreIcon />
+            </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+                {admin}
+            </CardContent>
+        </Collapse>
+            
         </Card>
                         
     );
