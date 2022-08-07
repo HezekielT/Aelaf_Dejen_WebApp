328617fd8d7dedc91e25d64ff53e1cae134e0447
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-07 19:02:55 +0300

    validation for convention form

--- client/src/components/EventsForm.js
+++ client/src/components/EventsForm.js
@@ -1,8 +1,67 @@
 import { Button, Container, Box, 
     Grid, Paper, Typography, TextField } from '@mui/material';
 import React from 'react';
+import { v4 as uuidV4 } from "uuid";
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const axios = require('axios');
+
+const conventionValidationSchema = yup.object({
+    title: yup
+      .string("Enter convention's title")
+      .required('Title is required'),
+    description: yup
+      .string('Enter description')
+      .required('Description is required'),
+    dateTime: yup
+      .string('Enter Time and Date of the convention')
+      .required('Date and Time of the convention is required'),
+    venue: yup
+      .string('Enter Venue')
+      .required('Venue is required'),
+});
 
 function EventsForm(props) {
+    const eid = uuidV4();
+
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            description: '',
+            dateTime: '',
+            venue: '',
+          },
+          validationSchema: conventionValidationSchema,
+          onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+            // e.preventDefault();
+            // const config = {
+            //     header: {
+            //         "Content-Type": "application/json"
+            //     },
+            // };
+            // const putvalues = async () => {
+            //     await axios.post(
+            //         "http://localhost:5000/addEvent",
+            //         {
+            //             id: eid,
+            //             title: values.title,
+            //             description: values.description,
+            //             dateTime: values.dateTime,
+            //             venue: values.venue,
+            //         },
+            //         config
+            //     ).then(function (){
+            //         alert(JSON.stringify("Successfully Added!"))
+            //     }).catch(function (error){
+            //         alert(error)
+            //     })
+            // }
+
+          }
+    });
+    
     return (
         <Container>
             <Grid container>
@@ -21,18 +80,39 @@ function EventsForm(props) {
 
                     <Paper sx={{width: '60vh'}}>
                         {/* <Card > */}
-                        <Box maxWidth="lg" sx={{backgroundColor: '#f5f5f5',p: 4,display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                        <Box component="form" onSubmit={formik.handleSubmit} maxWidth="lg" sx={{backgroundColor: '#f5f5f5',p: 4,display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
                             <Typography sx={{p: 2}}>
                                 Convention's Title
                             </Typography>
                             <TextField 
-                                variant='outlined'
-                                />
+                                required
+                                id="title"
+                                name="title"
+                                label="title"
+                                fullWidth
+                                autoComplete="title"
+                                variant="outlined"
+                                sx={{py: 1}}
+                                value={formik.values.title}
+                                onChange={formik.handleChange}
+                                error={formik.touched.title && Boolean(formik.errors.title)}
+                                helperText={formik.touched.title && formik.errors.title}
+                            />
                             <Typography sx={{p: 2}}>
                                 Description
                             </Typography>
                             <TextField 
-                                variant='outlined'
+                                required
+                                id="description"
+                                name="description"
+                                fullWidth
+                                autoComplete="description"
+                                variant="outlined"
+                                sx={{py: 1}}
+                                value={formik.values.description}
+                                onChange={formik.handleChange}
+                                error={formik.touched.description && Boolean(formik.errors.description)}
+                                helperText={formik.touched.description && formik.errors.description}
                             />
                             <Typography sx={{p: 2}}>
                                 Poster(if any)
@@ -55,16 +135,36 @@ function EventsForm(props) {
                                 Date and Time
                             </Typography>
                             <TextField 
-                                variant='outlined'
-                                />
+                                required
+                                id="dateTime"
+                                name="dateTime"
+                                fullWidth
+                                autoComplete="dateTime"
+                                variant="outlined"
+                                sx={{py: 1}}
+                                value={formik.values.dateTime}
+                                onChange={formik.handleChange}
+                                error={formik.touched.dateTime && Boolean(formik.errors.dateTime)}
+                                helperText={formik.touched.dateTime && formik.errors.dateTime}
+                            />
                             <Typography sx={{p: 2}}>
                                 Venue
                             </Typography>
                             <TextField 
-                                variant='outlined'
+                                required
+                                id="venue"
+                                name="venue"
+                                fullWidth
+                                autoComplete="venue"
+                                variant="outlined"
+                                sx={{py: 1}}
+                                value={formik.values.venue}
+                                onChange={formik.handleChange}
+                                error={formik.touched.venue && Boolean(formik.errors.venue)}
+                                helperText={formik.touched.venue && formik.errors.venue}
                             />
 
-                            <Button variant='contained' sx={{m: 2}}>Submit</Button>
+                            <Button type="submit" variant='contained' sx={{m: 2}}>Submit</Button>
                         </Box>
                         {/* </Card> */}
                     </Paper>
