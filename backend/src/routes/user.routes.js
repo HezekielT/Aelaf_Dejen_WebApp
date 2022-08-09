c92d8e64efa07fccdcecfacbc13e4ced5645cc29
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-09 14:43:26 +0300

    routes such as forgot password, reset password ... has been added

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,7 +2,9 @@ const { Participant } = require('../models/user.model');
 const { Driver } = require('../models/user.model')
 const { Admin } = require('../models/user.model')
 const { Router } = require("express");
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { sendResetEmail, sendConfirmationEmail } from "../config/nodemailer";
 
 const router = Router();
 // Routes related to convention participants
@@ -114,6 +116,8 @@ router.route("/deleteDriver/:id").delete(async (req, response) => {
 
 router.route('/registerAdmin').post(async function(req, res){
     const { id, first_name, last_name, email, phoneno, password, privilege } = req.body;
+    const token = jwt.sign({ email: email}, process.env.JWT_SECRET);
+
     try {
         await Admin.create({
             id,
@@ -125,32 +129,132 @@ router.route('/registerAdmin').post(async function(req, res){
             },
             password,
             privilege,
+            confirmationCode: token,
         });
         console.log("Successfully registered driver");
+        sendConfirmationEmail(first_name, last_name, email, privilege, token);
         res.status(201).send();
     } catch(err) {
         res.status(400).json({ message: err.message });
     }
-})
-// const { id, first_name, last_name, email, phoneno, password, privilege } = req.body;
-//     try {
-//         await Admin.create({
-//             id,
-//             admin: {
-//                 first_name,
-//                 last_name,
-//                 email,
-//                 phoneno,
-//             },
-//             plateno,
-//             location,
-//         });
-//         console.log("Successfully registered driver");
-//         res.status(201).send();
-//     } catch(err) {
-//         res.status(400).json({ message: err.message });
-//     }
 
+})
+
+router.route('/confirm/:confrimationCode').get(async function(req, res) {
+    const user = await Admin.findOne({
+        confirmationCode: req.params.confirmationCode,
+    })
+
+    if(!user) {
+        return res.status(404).send({ message: "Admin not found." });
+    }
+    user.status = "Active";
+    user.save((err) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+    });
+});
+
+router.route('/signin').post(async function(req, res) {
+    const { email, password } = req.body;
+    try {
+        const user = await Admin.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(404).send({ message: "Admin Not found!"});
+        }
+        const isMatch = await user.matchPassword(password);
+        if(!isMatch) {
+            return res.status(400).send({ message: "Incorrect email or password"})
+        }
+        res.status(200).send({
+            success: true,
+            data: "Successfully Logged in!",
+            token: user.getSignedJwtToken(),
+        });
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
+
+})
+
+router.route('/forgotPassword').post(async function(req, res, next){
+    const { email } = req.body;
+
+    try {
+        const user = await Admin.findOne({ email });
+        if (!user) {
+            return res.status(404).send({ message: "No email could not be sent, Please check your email!"})
+        }
+
+        const resetToken = user.getResetPasswordToken()
+
+        await user.save();
+        try {
+            sendResetEmail(user.email, resetToken);
+            res.status(200).json({ success: true, data: 'Email Sent'})
+        } catch (err) {
+            console.log(err);
+            user.resetPasswordToken= undefined;
+
+            await user.save();
+
+        }        
+    } catch (err) {
+        next(err)
+    }
+})
+// can the parameter reset token be optional?
+router.route('/passwordreset/:resetToken').post(async function(req, res){
+    if (req.params.resetToken !== null){
+        const resetPasswordToken = crypto
+            .createHash("sha256")
+            .update(req.params.resetToken)
+            .digest("hex");
+        try {
+            const user = await Admin.findOne({
+                resetPasswordToken,
+            });
+            if (!user) {
+                return res.status(400).send({ message: "Invalid Token!"})
+            }
+
+            user.password = req.body.password;
+            user.resetPasswordToken = undefined;
+
+            await user.save();
+            res.status(201).json({
+                success: true,
+                data: "Password Updated Successfully!",
+                token: user.getSignedJwtToken(),
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        const { email } = req.body.email
+        try {
+            const user = await Admin.findOne({
+                email,
+            });
+            if (!user) {
+                return res.status(400).send({ message: "Email Not Found!"})
+            }
+
+            user.password = req.body.password;
+
+            await user.save();
+            res.status(201).json({
+                success: true,
+                data: "Password Updated Successfully!",
+                token: user.getSignedJwtToken(),
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+})
 router.route('/getAdmins').get(async function(req, res) {
     try{
         const events = await Admin.find();
