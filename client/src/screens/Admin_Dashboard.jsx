40774122a48ae96d9cd993ef6f458a816a0fce25
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-11 17:58:39 +0300

    some cleaning up

--- client/src/screens/Admin_Dashboard.jsx
+++ client/src/screens/Admin_Dashboard.jsx
@@ -5,20 +5,21 @@ import Admin_Options from '../components/Admin/Admin_Options';
 import Manage_Contents from '../components/Admin/manage_web_content';
 
 const mdTheme = createTheme();
+const boxSx = {
+    backgroundColor: (theme) =>
+    theme.palette.mode === 'light'
+    ? theme.palette.grey[100]
+    : theme.palette.grey[900],
+    flexGrow: 1,
+    maxHeight: '100vh',
+    overflow: 'auto'
+}
 function AdminDashboard(props) {
     return (
         <ThemeProvider theme={mdTheme}>
             <Box
                 component="main"
-                sx={{
-                    backgroundColor: (theme) =>
-                    theme.palette.mode === 'light'
-                    ? theme.palette.grey[100]
-                    : theme.palette.grey[900],
-                    flexGrow: 1,
-                    maxHeight: '100vh',
-                    overflow: 'auto'
-                }}
+                sx={ boxSx }
             >
                 <Login />
                 {/* <Admin_Options /> */}
