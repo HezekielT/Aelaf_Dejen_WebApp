b7b0177ad0dc0f4e0572a26b4ba8855cdb8b2648
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-11 19:56:56 +0300

    route to reset password

--- client/src/Navigator.js
+++ client/src/Navigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ForgotPasswordForm from './components/Admin_Auth/ForgotPassword';
+import ResetPassword from './components/Admin_Auth/ResetPassword';
 import Admin_Options from './components/Admin_Tasks/Admin_Options';
 import Manage_Account from './components/Admin_Tasks/Manage_Account';
 import Manage_Contents from './components/Admin_Tasks/manage_web_content';
@@ -20,6 +21,7 @@ function Navigator(props) {
                 <Route path="/dashboard/transport" element={<Transport_Registration />} />
                 <Route path="/dashboard/accounts" element={<Manage_Account />} />
                 <Route path="/forgotpassword" element={<ForgotPasswordForm />} />
+                <Route path="/resetpassword/:resetToken?" element={<ResetPassword />} />
             </Routes>
         </Router>
     );
