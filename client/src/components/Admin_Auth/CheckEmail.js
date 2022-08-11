c22e282f24708d544eb59368fd72cdb055084512
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-11 18:29:08 +0300

    removed container

--- client/src/components/Admin_Auth/CheckEmail.js
+++ client/src/components/Admin_Auth/CheckEmail.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Container, Paper, Typography } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 
 function CheckEmail(props) {
   return (
-    <Container maxWidth="sm" sx={{pt: '8%'}}>  
-      <Paper
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
-        }}
-      >
-        <Typography comoponent="h1" variant='h5' sx={{p: 3}}>
-          Please Check Your Email Inbox!
-        </Typography>
-      </Paper>
-    </Container>
+    <Paper
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <Typography comoponent="h1" variant='h5' sx={{p: 3}}>
+        Please Check Your Email Inbox!
+      </Typography>
+    </Paper>
   );
 }
 
