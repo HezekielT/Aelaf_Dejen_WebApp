38ab40e38322cf497d1de696f77909d3e4e56406
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-16 00:08:47 +0300

    styling

--- client/src/components/Admin_Tasks/Admin_Options.js
+++ client/src/components/Admin_Tasks/Admin_Options.js
@@ -1,53 +1,60 @@
-import { Button, Container, Paper } from '@mui/material';
+import { Button, ThemeProvider, Paper, Box, createTheme } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+
+const mdTheme = createTheme()
+
+const boxSx = {
+
+    backgroundColor: (theme) =>
+    theme.palette.mode === 'light'
+    ? theme.palette.grey[100]
+    : theme.palette.grey[900],
+    flexGrow: 1,
+    minHeight: '83.6vh',
+    overflow: 'auto',
+    
+}
+const paperSx = {
+        m: 12,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        p: 5
+}
 
 function Admin_Options(props) {
     const navigate = useNavigate();
 
     return (
-        <Container maxWidth="lg" sx={{pt: '8%'}}>
+        <ThemeProvider theme={mdTheme}>
+            <Box
+                component="main"
+                sx={ boxSx }
+            >
             <Paper
-            sx={{
-                m: 6,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                p: 2
-            }}
+             sx={ paperSx }
             >
                 <Button variant="outlined" onClick={() => navigate('/dashboard/contents')}>
                     Manage website contents
                 </Button>
             </Paper>
             <Paper
-                sx={{
-                    m: 6,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    p: 2
-                }}
+                sx={ paperSx }
             >
                 <Button variant="outlined" onClick={() => navigate('/dashboard/transport')}>
                     Register Drivers
                 </Button>
             </Paper>
             <Paper
-                sx={{
-                    m: 6,
-                    mb: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    p: 2
-                }}
+                sx={ paperSx }
             >
                 <Button variant="outlined" onClick={() => navigate('/dashboard/accounts')}>
                     Manage Account
                 </Button>
             </Paper>
-        </Container>
+            </Box>
+        </ThemeProvider>
         // Manage website's content
         // Transport Providers registration
         // Manage Account
