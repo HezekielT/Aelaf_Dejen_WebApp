03209910c43731eef084807223f751eeeab701e1
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-16 00:09:47 +0300

    added some conditions

--- client/src/screens/navbar.jsx
+++ client/src/screens/navbar.jsx
@@ -2,21 +2,56 @@ import React, { useState } from 'react';
 import { AppBar, ThemeProvider, Box, 
     CssBaseline, Container, Toolbar, 
     Typography, createTheme, IconButton, 
-     Menu, MenuItem, Link } from '@mui/material';
+     Menu, MenuItem, Link, ListItemIcon, Avatar, Divider, Stack, Tooltip } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu'
 import { useEffect } from 'react';
 import { useReference } from '../context/refProvider';
-import Home from '../components/Home'
+import { Logout } from '@mui/icons-material';
+import { useLocation, useNavigate } from 'react-router-dom';
+// import { LogoutIcon } from '@mui/icons-material/Logout'
 
 const pages = ['Home', 'Conventions', 'About Us']
 const theme = createTheme();
+
+function stringToColor(string) {
+    let hash = 0;
+    let i;
+  
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+  
+    let color = '#';
+  
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+  
+    return color;
+  }
+  
+  function stringAvatar(name) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    };
+  }
+
 function NavBar() {
+    const location = useLocation();
+    const navigate = useNavigate();
     const [isScrolled, setIsScrolled ] = useState('#1565c0');
     const { homeRef, eventRef, contactRef, scrollToRef } = useReference();
-    const [anchorElPro, setAnchorElPro] = useState(null);
-    // const open = Boolean(anchorElPro);
 
+    const [anchorElPro, setAnchorElPro] = useState(null);
+    const [anchorEl, setAnchorEl] = useState(null)
     const [anchorElNav, setAnchorElNav] = useState(null);
+    const open = Boolean(anchorEl);
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
@@ -24,13 +59,12 @@ function NavBar() {
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
-      };
-      
-      
+    };
+
     const handleClick = (event) => {
         console.log("gag")
         setAnchorElPro(event.currentTarget);
-        console.log(anchorElPro)
+        // console.log(anchorElPro)
     }
     const handleClose = () => {
         setAnchorElPro(null);
@@ -51,95 +85,196 @@ function NavBar() {
             window.removeEventListener('scroll', handleScroll)
         }
     }, [])
+    
+    function AdminXsNavBar() {
+        return (
+            <>
+            
+                <ListItemIcon sx={{ mx :1 }} onClick={() => {console.log("JJJ")}}>
+                    <Logout fontSize='small' sx={{ ml:1 }}/>
+                    <Typography>Logout</Typography>
+                </ListItemIcon>
+            
+            </>
+        )
+    }
 
+    function AdminMdNavBar() {
+        // const full_name = location.state.user.first_name + ' ' + location.state.user.last_name
+        return (
+            <React.Fragment>
+            <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            >
+                <Stack direction="row" spacing={2}>
+                    <Avatar  {...stringAvatar("full name")} />
+                </Stack>
+            </IconButton>
+            <Menu
+                anchorEl={anchorElPro}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem
+                    onClick={() => navigate('/logout')}
+                >
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+            </React.Fragment>
+        )
+    }
+
+    function NormalXsNavBar() {
+        return (
+            <>
+            <MenuItem key={0} onClick={handleCloseNavMenu}>
+                <Link sx={{cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(homeRef)}>Home</Link>
+            </MenuItem>
+            <MenuItem key={1} onClick={handleCloseNavMenu}>
+                <Link sx={{cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(eventRef)}>Conventions</Link>
+            </MenuItem>
+            <MenuItem key={2} onClick={handleCloseNavMenu}>
+                <Link sx={{cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(contactRef)}>Contact Us</Link>
+            </MenuItem>
+            </>
+        )
+    }
+    
+    function NormalMdNavBar() {
+        return (
+            <>
+                <Link 
+                    sx={{
+                        color: '#fff', 
+                        cursor: 'pointer', 
+                        textDecoration: 'none',
+                        px: 2,
+                    }} 
+                    onClick={() => scrollToRef(homeRef)}
+                >Home</Link>
+                <Link sx={{color: '#fff', cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(eventRef)}>Conventions</Link>
+                <Link sx={{color: '#fff', cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(contactRef)}>Contact Us</Link>
+            </>
+        )
+    }
     const cc = "#322162"
+    function Content() {
+    	if(location.pathname === '/' || location.pathname === '/dashboard' 
+        || location.pathname === '/dashboard/content' || location.pathname === '/dashboard/transport' 
+        || location.pathname === '/dashboard/accounts') {
+            return(
+                    <Box sx={{display: 'flex'}}>
+                        <CssBaseline />
+                        <AppBar style={{background: isScrolled}}>
+                            <Container maxWidth="xl">
+                                <Toolbar>
+                                    <Typography
+                                        variant='h6'
+                                        noWrap
+                                        component="div"
+                                        sx={{ flexGrow: 1, display: {xs: 'none', md: 'flex'} }}
+                                    >
+                                        <pre>
+                                        አእላፍ-ደጀን 
+                                        AELAPH-DEJEN
+                                        </pre>
+                                    </Typography>
+                                    <Box sx={{ flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                                        <IconButton
+                                            size="large"
+                                            aria-label='drivers option'
+                                            aria-controls='menu-appbar'
+                                            aria-haspopup="true"
+                                            onClick={handleOpenNavMenu}
+                                        >
+                                            <MenuIcon />
+                                        </IconButton>
+                                        <Menu
+                                            id="menu-appbar"
+                                            anchorEl={anchorElNav}
+                                            anchorOrigin={{
+                                                vertical: 'bottom',
+                                                horizontal: 'left',
+                                            }}
+                                            keepMounted
+                                            transformOrigin={{
+                                                vertical: 'top',
+                                                horizontal: 'left',
+                                            }}
+                                            open={Boolean(anchorElNav)}
+                                            onClose={handleCloseNavMenu}
+                                            sx={{
+                                                display: { xs: 'block', md: 'none' },
+                                            }}
+                                        > 
+                                                {location.pathname === '/' ? (
+                                                    <NormalXsNavBar />
+                                                ) : (
+                                                    <AdminXsNavBar />
+                                                )}
+                                        </Menu>
+                                    </Box>
+                                    <Typography
+                                        variant='h6'
+                                        noWrap
+                                        component="div"
+                                        sx={{
+                                            flexGrow: 1,
+                                            display: { xs: 'flex', md: 'none'}
+                                        }}
+                                    >
+                                        AELAPH-DEJEN
+                                    </Typography>
+                                    <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+                                        {/* <Link 
+                                            sx={{
+                                                color: '#fff', 
+                                                cursor: 'pointer', 
+                                                textDecoration: 'none',
+                                                px: 2,
+                                            }} 
+                                            onClick={() => scrollToRef(homeRef)}
+                                        >Home</Link>
+                                        <Link sx={{color: '#fff', cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(eventRef)}>Conventions</Link>
+                                        <Link sx={{color: '#fff', cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(contactRef)}>Contact Us</Link> */}
+                                        {location.pathname === '/' ? (
+                                            <NormalMdNavBar />
+                                        ) : (
+                                            <AdminMdNavBar />
+                                        )}
+                                    </Box>
+                                </Toolbar>
+                            </Container>
+                        </AppBar>
+                    </Box>
+            )
+             } else {
+              return (<></>)
+             }
+    }
+
+    useEffect(() => {
+       <Content />
+    },[location.pathname])
+
     return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{display: 'flex'}}>
-                <CssBaseline />
-                <AppBar style={{background: isScrolled}}>
-                    <Container maxWidth="xl">
-                        <Toolbar>
-                            <Typography
-                                variant='h6'
-                                noWrap
-                                component="div"
-                                sx={{ flexGrow: 1, display: {xs: 'none', md: 'flex'} }}
-                            >
-                                <pre>
-                                 አእላፍ-ደጀን 
-                                AELAPH-DEJEN
-                                </pre>
-                            </Typography>
-                            <Box sx={{ flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
-                                <IconButton
-                                    size="large"
-                                    aria-label='drivers option'
-                                    aria-controls='menu-appbar'
-                                    aria-haspopup="true"
-                                    onClick={handleOpenNavMenu}
-                                >
-                                    <MenuIcon />
-                                </IconButton>
-                                <Menu
-                                    id="menu-appbar"
-                                    anchorEl={anchorElNav}
-                                    anchorOrigin={{
-                                        vertical: 'bottom',
-                                        horizontal: 'left',
-                                    }}
-                                    keepMounted
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'left',
-                                    }}
-                                    open={Boolean(anchorElNav)}
-                                    onClose={handleCloseNavMenu}
-                                    sx={{
-                                        display: { xs: 'block', md: 'none' },
-                                    }}
-                                > 
-                                        <MenuItem key={0} onClick={handleCloseNavMenu}>
-                                            <Link sx={{cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(homeRef)}>Home</Link>
-                                        </MenuItem>
-                                        <MenuItem key={1} onClick={handleCloseNavMenu}>
-                                            <Link sx={{cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(eventRef)}>Conventions</Link>
-                                        </MenuItem>
-                                        <MenuItem key={2} onClick={handleCloseNavMenu}>
-                                            <Link sx={{cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(contactRef)}>Contact Us</Link>
-                                        </MenuItem>
-                             </Menu>
-                            </Box>
-                            <Typography
-                                variant='h6'
-                                noWrap
-                                component="div"
-                                sx={{
-                                    flexGrow: 1,
-                                    display: { xs: 'flex', md: 'none'}
-                                }}
-                            >
-                                AELAPH-DEJEN
-                            </Typography>
-                            <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-                                <Link 
-                                    sx={{
-                                        color: '#fff', 
-                                        cursor: 'pointer', 
-                                        textDecoration: 'none',
-                                        px: 2,
-                                    }} 
-                                    onClick={() => scrollToRef(homeRef)}
-                                >Home</Link>
-                                <Link sx={{color: '#fff', cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(eventRef)}>Conventions</Link>
-                                <Link sx={{color: '#fff', cursor: 'pointer', textDecoration: 'none',px: 2,}} onClick={() => scrollToRef(contactRef)}>Contact Us</Link>
-                            </Box>
-                        </Toolbar>
-                    </Container>
-                </AppBar>
-            </Box>
-        </ThemeProvider>
-    );
+    	<ThemeProvider theme={theme}>
+    		<Content />
+    	</ThemeProvider>
+    )
 }
 
 export default NavBar;
