dc0f360aea52979a28369ea3faf41468dafb7ad6
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-16 18:48:58 +0300

    changed to support private routes

--- client/src/Navigator.js
+++ client/src/Navigator.js
@@ -12,6 +12,7 @@ import NavBar from '../src/screens/navbar';
 import Footer from '../src/screens/footer';
 import Not_Found from './screens/Not_Found';
 import Create_New_Password from './components/Admin_Tasks/Create_New_Password';
+import PrivateRoute from './Routes/PrivateRoute'
 
 function Navigator(props) {
     
@@ -19,15 +20,23 @@ function Navigator(props) {
         <Router>
             <NavBar />
                 <Routes>
-                    <Route exact path="/" element={<Dashboard />} />
+                    <Route exact path='/' element={<Dashboard />} />
                     <Route path="/admin" element={<AdminDashboard />} />
-                    <Route path="/dashboard" element={<Admin_Options />} />
-                    <Route path="/dashboard/contents" element={<Manage_Contents />} />
-                    <Route path="/dashboard/transport" element={<Transport_Registration />} />
-                    <Route path="/dashboard/accounts" element={<Manage_Account />} />
+                    <Route exact path="/dashboard" element={<PrivateRoute />}>
+                        <Route exact path="/dashboard" element={<Admin_Options />} />
+                    </Route>
+                    <Route path="/dashboard/contents" element={<PrivateRoute />}>
+                        <Route path="/dashboard/contents" element={<Manage_Contents />} />
+                    </Route>
+                    <Route path="/dashboard/transport" element={<PrivateRoute />}>
+                        <Route path="/dashboard/transport" element={<Transport_Registration />} />
+                    </Route>
+                    <Route path="/dashboard/accounts" element={<PrivateRoute />}>
+                        <Route path="/dashboard/accounts" element={<Manage_Account />} />
+                    </Route>
                     <Route path="/forgotpassword" element={<ForgotPasswordForm />} />
                     <Route path="/resetpassword/:resetToken?" element={<ResetPassword />} />
-                    <Route path='/confirm/?confirmationCode' element={<Create_New_Password />} />
+                    <Route path='/confirm/:confirmationCode' element={<Create_New_Password />} />
                     <Route path="*" element={<Not_Found />} />
                 </Routes>
             <Footer />
