5191e325d0da210aeeddbc432c149763baa246d9
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-17 05:14:23 +0300

    forgot to uncomment some comments

--- index.js
+++ index.js
@@ -36,13 +36,13 @@ app.use('/api/driver/',require('./src/routes/user.routes'));
 app.use('/api/event/',require('./src/routes/events.routes'));
 app.use('/api/admin',admin);
 
-// if(process.env.NODE_ENV ===  'production') {
-//     app.use(express.static('client/build'))
+if(process.env.NODE_ENV ===  'production') {
+    app.use(express.static('client/build'))
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     } )
-// }
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    } )
+}
 
 app.listen(port, () => {
     console.log(`Currently Listening at http://localhost:${port}`);
