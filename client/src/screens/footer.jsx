1db90befbc0f8c25e5f6ba029c6ae42c0b4fdd04
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-17 23:19:41 +0300

    footer content change

--- client/src/screens/footer.jsx
+++ client/src/screens/footer.jsx
@@ -8,137 +8,9 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedinIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
-const navigate = useNavigate;
-function Copyright() {
-    return (
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{pl: 3,display: 'flex', justifyContent: 'center' }}>
-            <Typography variant='body2' color='text.secondary'>
-                {'Copyright ©  '}
-                <Link color="inherit" onClick={() => navigate('/')}>
-                    AELAPH-DEJEN
-                </Link>
-                {' '}
-                {new Date().getFullYear()}
-                {'.'}
-            </Typography>
-        </Grid>
-    );
-}
-
-function Addresss() {
-    return (
-        // <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-            <>
-                <Typography sx={{ pl:2 }}> &gt; Address</Typography>
-                <Typography sx={{}}>
-            U.S
-            </Typography>
-            <Typography sx={{ 
-                // pl: 2
-            }}>
-            +1 (469) 432-2435
-            </Typography>
-            </>
-            
-        // </Grid>
-    )
-}
-
-function Usefullinks() {
-    return (
-        // <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-        <>
-            <Typography sx={{ 
-                pl: 2, 
-                color: '#64748B'}}>
-            &gt; Useful links
-            </Typography>
-            <Box 
-                variant='body1' 
-                sx={{ 
-                    display: 'flex',
-                    flexDirection: 'column',
-                    // pt: 2,
-                    // px: 10,
-                    cursor: 'pointer'
-                }}
-                >
-                {/* <Link onClick={() => navigate('/aboutus')}>
-                    &gt; About Us
-                </Link>
-                <Link onClick={() => navigate('/aboutus')}>
-                    &gt; Contact Us
-                </Link> */}
-                <Link onClick={() => navigate('/aboutus')}>
-                    Terms of Service
-                </Link>
-                <Link onClick={() => navigate('/aboutus')}>
-                    Privacy Policiy
-                </Link>
-            </Box>
-        </>
-        // </Grid>
-    )
-}
-
-function SocialLinks() {
-    return (
-        // <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-        <>
-            <Typography sx={{ pl: 2}}>
-            &gt; Our Social Links
-            </Typography>
-                        
-            <Grid item xs={4} sm={2} md={4} lg={4} xl={4}
-                sx={{
-                    pt: 1,
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-around',
-                    // mx: 3
-                }}
-                >
-                <Grid item>
-                    <Link >
-                        <GoogleIcon/>
-                    </Link>
-                </Grid>
-                <Grid item>
-                    <Link>
-                        <FacebookIcon />
-                    </Link>
-                </Grid>
-                <Grid item>
-                    <Link>
-                        <TwitterIcon />
-                    </Link>
-                </Grid>
-                <Grid item>
-                    <Link>
-                        <LinkedinIcon />
-                    </Link>
-                </Grid>
-                <Grid item>
-                    <Link>
-                        <InstagramIcon />
-                    </Link>
-                </Grid>
-            
-            </Grid>
-            </>
-        // </Grid>
-
-    )
-}
-
-// function Component() {
-    //     return (
-        //         footer
-        //         )
-        //     }
 function Footer(props) {
-    const location = useLocation()
-    
+    const navigate = useNavigate();
+    const location = useLocation();
     const [footer, setFooter] = useState(null)
     
     useEffect(() => {
@@ -160,7 +32,7 @@ function Footer(props) {
                         ? theme.palette.grey[200] 
                         : theme.palette.grey[800],
                     }}
-                >
+                    >
                     <Container maxWidth="xl">
                         <Grid container spacing={2}>
                             <Grid item xs={12} md={12} sx={{ pl: 3,display: 'flex', justifyContent: 'center' }}>
@@ -168,13 +40,13 @@ function Footer(props) {
                                     AELAPH-DEJEN CONVENTION
                                 </Typography>
                             </Grid>
-                            <Grid item xs={12} md={4}>
+                            <Grid item xs={12} md={4} sx={{ justifyContent: {xs: 'center'}, display: {xs: 'flex'}, }}>
                                 <Addresss />
                             </Grid>
-                            <Grid item xs={12} md={4}>
+                            <Grid item xs={12} md={4} sx={{ justifyContent: {xs: 'center'}, display: {xs: 'flex'}, }}>
                                 <Usefullinks />
                             </Grid>
-                            <Grid item xs={12} md={4}>
+                            <Grid item xs={12} md={4} sx={{ justifyContent: {xs: 'center'}, display: {xs: 'flex'}, }}>
                                 <SocialLinks />
                             </Grid>
                             {/* <Grid item xs={12} md={4}> */}
@@ -188,8 +60,155 @@ function Footer(props) {
             setFooter( <></> )
             
         }
-    },[location.pathname])
-        
+    },[location.pathname])    
+
+    function Usefullinks() {
+        return (
+            // <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+            <Box
+                variant='body1' 
+                sx={{ 
+                    display: 'flex',
+                    flexDirection: 'column',
+                    // pt: 2,
+                    // px: 10,
+                    cursor: 'pointer'
+                }}
+                >
+                <Typography sx={{ 
+                    pl: 2, 
+                    color: '#64748B'}}>
+                &gt; Useful links
+                </Typography>
+                <Box 
+                    variant='body1' 
+                    sx={{ 
+                        display: 'flex',
+                        flexDirection: 'column',
+                        // pt: 2,
+                        // px: 10,
+                        cursor: 'pointer'
+                    }}
+                    >
+                    {/* <Link onClick={() => navigate('/aboutus')}>
+                        &gt; About Us
+                    </Link>
+                    <Link onClick={() => navigate('/aboutus')}>
+                        &gt; Contact Us
+                    </Link> */}
+                    <Link onClick={() => navigate('/aboutus')}>
+                        Terms of Service
+                    </Link>
+                    <Link onClick={() => navigate('/aboutus')}>
+                        Privacy Policiy
+                    </Link>
+                </Box>
+            </Box>
+            // </Grid>
+        )
+    }
+
+    function SocialLinks() {
+        return (
+            // <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+            <Box
+            variant='body1' 
+            sx={{ 
+                display: 'flex',
+                    flexDirection: 'column',
+                    // pt: 2,
+                    // px: 10,
+                    cursor: 'pointer'
+                }}
+                >
+                <Typography sx={{ pl: 2}}>
+                &gt; Our Social Links
+                </Typography>
+                            
+                <Grid item xs={4} sm={2} md={4} lg={4} xl={4}
+                    sx={{
+                        pt: 1,
+                        display: 'flex',
+                        flexDirection: 'row',
+                        // justifyContent: 'space-around',
+                        // mx: 3
+                    }}
+                    >
+                    <Grid item>
+                        <Link >
+                            <GoogleIcon/>
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link>
+                            <FacebookIcon />
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link>
+                            <TwitterIcon />
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link>
+                            <LinkedinIcon />
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link>
+                            <InstagramIcon />
+                        </Link>
+                    </Grid>
+                
+                </Grid>
+                </Box>
+    
+        )
+    }
+
+    function Addresss() {
+        return (
+            // <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                <Box
+                variant='body1' 
+                sx={{ 
+                    display: 'flex',
+                    flexDirection: 'column',
+                    // pt: 2,
+                    // px: 10,
+                    cursor: 'pointer'
+                }}
+                >
+                    <Typography sx={{ pl:2, }}> &gt; Address</Typography>
+                    <Typography sx={{}}>
+                U.S
+                </Typography>
+                <Typography sx={{ 
+                    // pl: 2
+                }}>
+                +1 (469) 432-2435
+                </Typography>
+                </Box>
+                
+            // </Grid>
+        )
+    }
+    
+    function Copyright() {
+        return (
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{pl: 3,display: 'flex', justifyContent: 'center' }}>
+                <Typography variant='body2' color='text.secondary'>
+                    {'Copyright ©  '}
+                    <Link color="inherit" onClick={() => navigate('/')}>
+                        AELAPH-DEJEN
+                    </Link>
+                    {' '}
+                    {new Date().getFullYear()}
+                    {'.'}
+                </Typography>
+            </Grid>
+        );
+    }
 
     return (
         footer
