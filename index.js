e95ea41e7ab84deed0d6738c89d9224986307529
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-17 05:08:38 +0300

    api added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require('path')
 
 require('dotenv').config();
 
+const admin = require('./src/routes/admin.routes')
+
 const app = express();
 
 const DATABASE_CONNECTION = process.env.DATABASE_URL;
@@ -30,17 +32,17 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.use(require('./src/routes/user.routes'));
-app.use(require('./src/routes/events.routes'));
-app.use(require('./src/routes/admin.routes'));
+app.use('/api/driver/',require('./src/routes/user.routes'));
+app.use('/api/event/',require('./src/routes/events.routes'));
+app.use('/api/admin',admin);
 
-if(process.env.NODE_ENV ===  'production') {
-    app.use(express.static('client/build'))
+// if(process.env.NODE_ENV ===  'production') {
+//     app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    } )
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     } )
+// }
 
 app.listen(port, () => {
     console.log(`Currently Listening at http://localhost:${port}`);
