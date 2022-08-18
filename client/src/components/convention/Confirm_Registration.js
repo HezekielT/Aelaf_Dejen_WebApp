ff3f7e274b071b61122f0a1d813b4c241d23cdb1
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-18 18:31:06 +0300

    modal works now

--- client/src/components/convention/Confirm_Registration.js
+++ client/src/components/convention/Confirm_Registration.js
@@ -3,36 +3,35 @@ import Modal from '@mui/material/Modal'
 import { Typography, Box } from '@mui/material';
 
 function ConfirmRegistration(props) {
-    const [open, setOpen] = React.useState(false);
-    // console.log(props.val)
-    useEffect(() => {
+    // const [open, setOpen] = React.useState(false);
+    // // console.log(props.val)
+    // useEffect(() => {
 
-        if(props.val == true){
-            handleOpen()
-        }
-    },[])
-    const handleOpen = () => {
-        setOpen(true)
-    };
-    const handleClose = () => {
+    //     if(props.val == true){
+    //         handleOpen()
+    //     }
+    // },[])
+    // const handleOpen = () => {
+    //     setOpen(true)
+    // };
+    // const handleClose = () => {
         
-    };
+    // };
 
     return (
         <React.Fragment>
             <Modal
                 open={props.open}
-                onClose={handleClose}
+                onClose={() => {props.setOpen(false)}}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
                 >
-                <Box>
+                <Box position='absolute' top="50%" left="50%">
                     <Typography id="modal-modal-title" sx={{ mt: 2 }}>
-                        Thank You for Registering!!
+                        Success
                     </Typography>
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        You have Successfully registered for the upcoming convention.
-                        Updates will be sent to you!
+                        {props.message}
                     </Typography>
                 </Box>
             </Modal>
