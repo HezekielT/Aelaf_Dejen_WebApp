1b75ce9543c63202c92e874a2958d08f901a62dc
hizkielT <hezekiel.feyissa@gmail.com>
2022-08-25 16:38:39 +0300

    New component

--- client/src/components/SuccessComponent.js
+++ client/src/components/SuccessComponent.js
@@ -1,8 +1,16 @@
+import { Alert, Snackbar } from '@mui/material';
 import React from 'react';
 
 function SuccessComponent(props) {
   return (
-    <></>
+    <Snackbar
+      open={props.isOpen}
+      autoHideDuration={3000}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right'}}
+      onClose={handleClose}
+    >
+      <Alert severity={props.type}>{props.message}</Alert>
+    </Snackbar>
   );
 }
 
